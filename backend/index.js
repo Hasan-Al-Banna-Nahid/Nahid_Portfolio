7af7ec2b5324da7d568e2d7e8ac285c5c442781c
Hasan-Al-Banna-Nahid <iamnahid591998@gmail.com>
2025-03-22 20:28:03 +0600

    Initial Setup with routes and sidebar dynamic with redux setup complete

--- backend/index.js
+++ backend/index.js
@@ -15,17 +15,16 @@ app.use(
   })
 );
 // Manually set CORS headers
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://nahid-portfolio-mu.vercel.app"
-  ); // Your frontend URL
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS"); // Allowed methods
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Allowed headers
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader(
+//     "Access-Control-Allow-Origin",
+//     "https://nahid-portfolio-mu.vercel.app"
+//   ); // Your frontend URL
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS"); // Allowed methods
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Allowed headers
+//   next();
+// });
 // Handle Preflight (OPTIONS) Requests
-app.options("*", cors());
 
 // Middleware
 app.use(express.json());
